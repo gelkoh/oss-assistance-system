7aba10c0badceb872d692e416f2e206e3755dd95
gelkoh <ole_kirchner@gmx.de>
2025-11-11 19:28:31 +0100

    Give CanvasControls the new ui-scheme

--- src/components/CanvasControls.vue
+++ src/components/CanvasControls.vue
@@ -1,15 +1,28 @@
 <template>
-    <div class="absolute bottom-6 right-6 bg-neutral-500 border border-neutral-500 rounded-md flex gap-x-[1px] h-8">
-        <button @click="$emit('expand-all')" class="px-2 cursor-pointer bg-neutral-800 hover:bg-neutral-700 rounded-l-md">
-            Expand all
+    <div
+        class="absolute bottom-4 right-4 bg-neutral-800 rounded-md flex gap-x-2
+               p-1"
+    >
+        <button
+            @click="$emit('collapse-all')"
+            class="flex items-center gap-x-2 px-2 cursor-pointer bg-neutral-800
+                   hover:bg-neutral-700 h-8 rounded-sm active:bg-neutral-600"
+        >
+            <FoldVertical :size="22" />
+            Collapse all
         </button>
 
-        <button @click="$emit('collapse-all')" class="px-2 cursor-pointer bg-neutral-800 hover:bg-neutral-700 rounded-r-md">
-            Collapse all
+        <button
+            @click="$emit('expand-all')"
+            class="flex items-center gap-x-2 px-2 cursor-pointer bg-neutral-800
+                   hover:bg-neutral-700 h-8 rounded-sm active:bg-neutral-600"
+        >
+            <UnfoldVertical :size="22" />
+            Expand all
         </button>
     </div>
 </template>
 
 <script setup>
-
+    import { FoldVertical, UnfoldVertical } from "lucide-vue-next"
 </script>
